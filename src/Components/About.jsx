7b2710d2545536943d9dd4091025dde5ee633e1c
Melchizedek Amasai <94358022+IamAmasai@users.noreply.github.com>
2024-01-04 15:15:08 +0000

    committed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a student at the Technical University of Kenya passionate about making impact through tech and transformational leadership.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Mobile app development using Flutter",
+  "Business intelligence and analytics",
+  "Cloud solutions architect",
+  "Relational and Transformational leadership",
+  "",
 ];
 
 /**
