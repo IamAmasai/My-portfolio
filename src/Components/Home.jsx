b1b3158c19b3314962b77c95570b59d4263cc65e
Melchizedek Amasai <94358022+IamAmasai@users.noreply.github.com>
2024-01-11 08:32:30 +0000

    changes in home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,28 +22,34 @@ import PropTypes from "prop-types";
  */
 import image from "../images/socials/IMG_0681.jpg";
 
-const imageAltText = "";
+// Alt text for the image
+const imageAltText = "Juma Melchizedek Amasai";
 
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
+      {/* Background image */}
+      <img className="background" src={image} alt={imageAltText} />
+      {/* Name and title */}
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
+      {/* Arrow image */}
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt="Arrow pointing down" />
       </div>
     </section>
   );
 };
 
+// Default props
 Home.defaultProps = {
   name: "Juma Melchizedek Amasai",
   title: "CEO, Eco-labs Africa",
 };
 
+// Prop types
 Home.propTypes = {
   name: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
